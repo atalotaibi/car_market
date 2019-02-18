cf245bc0f6ef7b67f4ca4000cf1c06417da4cd3c
atalotaibi <alotaibiatallah@gmail.com>
2019-02-18 11:39:38 +0300

    car images add to the detail

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Car
+from .forms import CarForm
+from django.contrib import messages
 
 def car_list(request):
 	cars = Car.objects.all()
@@ -18,15 +20,38 @@ def car_detail(request, car_id):
 
 
 def car_create(request):
-	#Complete Me
-	return render(...)
+	form = CarForm()
+	if request.method == "POST":
+		form = CarForm(request.POST, request.FILES)
+		if form.is_valid():
+			form.save()
+			messages.success(request, 'Successfully created')
+			return redirect('car-list')
+	context = {
+		"car": form,
+		}
+	return render(request, 'car_create.html', context)
 
 
 def car_update(request, car_id):
+	car_obj = Car.objects.get(id=car_id)
+	car = CarForm(instance=car_obj)
+	if request.method == "POST":
+		car = CarForm(request.POST, instance=car_obj)
+		if car.is_valid():
+			car.save()
+			messages.success(request, 'Successfully updated.')
+			return redirect('car-list')
+	context = {
+        "car_obj": car_obj,
+        "car": car,
+    }
 	#Complete Me
-	return render(...)
+	return render(request, 'car_update.html', context)
 
 
 def car_delete(request, car_id):
+	car_obj = Car.objects.get(id=car_id)
+	car_obj.delete()
 	#Complete Me
-	return render(...)
+	return redirect('car-list')
